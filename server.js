6ec0c45a4f73e66a84394fcdd886493243a46071
LIONEL SCHWARZ <lionelschwarz@MacBookPro.local.tld>
2026-02-23 09:58:27 +0100

    server: favor accuracy with sequential scrape and longer wait

--- server.js
+++ server.js
@@ -99,8 +99,8 @@ function scrapeMetrics() {
       '--json=true',
       '--headless=true',
       '--source=direct',
-      '--wait-ms=1500',
-      '--concurrency=3',
+      '--wait-ms=2800',
+      '--concurrency=1',
       ...VIDEOS.map((v) => `--url=${v.url}`),
     ];
     const sessionFile = resolveSessionFile();
